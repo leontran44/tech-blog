91d5c4c09e8e7cd35722ab9ef9fc80c7b2bb4987
Leon <tranhoangquan44@gmail.com>
2024-09-09 22:57:07 +1000

    regenerate data and seeding

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,35 +1,35 @@
-const sequelize = require('../config/connection');
-const { User, Post, Comment } = require('../models');
-const fs = require('fs');
-const path = require('path');
+const sequelize = require("../config/connection");
+const { User, Post, Comment } = require("../models");
+const fs = require("fs");
+const path = require("path");
 
 // Seed the database
 const seedAll = async () => {
-await sequelize.sync({ force: true }); // Drops existing tables and recreates them
+  await sequelize.sync({ force: true }); // Drops existing tables and recreates them
 
-console.log('--- DATABASE SYNCED ---');
+  console.log("--- DATABASE SYNCED ---");
 
-// Read JSON data
-const users = readJSON('userData.json');
-const posts = readJSON('postData.json');
-const comments = readJSON('commentData.json');
+  // Read JSON data
+  const userData = require("./userData.json");
+  const postData = require("./postData.json");
+  const commentData = require("./commentData.json");
 
-// Seed Users
-await User.bulkCreate(users, {
+  // Seed Users
+  await User.bulkCreate(User, {
     individualHooks: true, // Ensures password hashing happens
     returning: true,
-});
-console.log('--- USERS SEEDED ---');
+  });
+  console.log("--- USERS SEEDED ---");
 
-// Seed Posts
-await Post.bulkCreate(posts);
-console.log('--- POSTS SEEDED ---');
+  // Seed Posts
+  await Post.bulkCreate(Post);
+  console.log("--- POSTS SEEDED ---");
 
-// Seed Comments
-await Comment.bulkCreate(comments);
-console.log('--- COMMENTS SEEDED ---');
+  // Seed Comments
+  await Comment.bulkCreate(Comment);
+  console.log("--- COMMENTS SEEDED ---");
 
-process.exit(0); // Exit the process once seeding is complete
+  process.exit(0); // Exit the process once seeding is complete
 };
 
 seedAll();
