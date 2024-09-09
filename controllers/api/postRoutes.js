9df803cf7dc5acf4667bdb20dd12f4d4f2f9d1cf
Leon <tranhoangquan44@gmail.com>
2024-09-09 21:56:15 +1000

    configurate a create new post and fetch

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const { Post } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Create new post
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+    
+        res.status(200).json(newPost);
+        } catch (err) {
+            res.status(500).json(err);
+    }
+});
+
+// Fetch posts
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll();
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+module.exports = router;
