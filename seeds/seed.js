19ab300acab410b393fd9d9b2edf02a42b54ae05
Leon <115925556+leontran44@users.noreply.github.com>
2024-09-17 02:49:11 +0000

    fix bugs where seeds were not generated

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,32 +1,33 @@
 const sequelize = require("../config/connection");
 const { User, Post, Comment } = require("../models");
-const fs = require("fs");
-const path = require("path");
 
+const userData = require("./userData.json");
+const postData = require("./postData.json");
+const commentData = require("./commentData.json");
 // Seed the database
+
 const seedAll = async () => {
   await sequelize.sync({ force: true }); // Drops existing tables and recreates them
 
   console.log("--- DATABASE SYNCED ---");
 
-  // Read JSON data
-  const userData = require("./userData.json");
-  const postData = require("./postData.json");
-  const commentData = require("./commentData.json");
-
   // Seed Users
-  await User.bulkCreate(User, {
+  const users = await User.bulkCreate(userData, {
     individualHooks: true, // Ensures password hashing happens
     returning: true,
   });
   console.log("--- USERS SEEDED ---");
 
-  // Seed Posts
-  await Post.bulkCreate(Post);
-  console.log("--- POSTS SEEDED ---");
+   // Seed Posts with random user_id assignment
+   for (const post of postData) {
+    await Post.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id, // Assign random user_id
+    });
+  }
 
   // Seed Comments
-  await Comment.bulkCreate(Comment);
+  await Comment.bulkCreate(commentData);
   console.log("--- COMMENTS SEEDED ---");
 
   process.exit(0); // Exit the process once seeding is complete
