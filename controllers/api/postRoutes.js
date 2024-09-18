5babc0e0766ce4f4c8a28575a36d0867ebcebddf
Leon <115925556+leontran44@users.noreply.github.com>
2024-09-18 22:51:29 +0000

    fix get single post by delete render post handlebars

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -30,29 +30,18 @@ router.get("/", async (req, res) => {
 });
 
 // Get a single post by ID
-router.get("/:id", async (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
       include: [{ model: User }, { model: Comment }],
     });
 
-        if (!postData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-        }
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
 
     res.status(200).json(postData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-    const post = postData.get({ plain: true });
-
-    res.render("post", {
-      post,
-      logged_in: req.session.logged_in,
-    });
   } catch (err) {
     res.status(500).json(err);
   }
